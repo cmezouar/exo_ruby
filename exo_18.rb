a8d62b9e1b3674b7b8e31202175fa16edaedb58f
assouev <bsngr.asv@gmail.com>
2019-10-01 18:48:49 +0200

    exo 18

--- exo_18.rb
+++ exo_18.rb
@@ -1,9 +1,14 @@
 a = 0
 my_array = []
 50.times do
-	a = a + 1
-	email = "jean.dupont0#{a}@email.fr"
-	puts email
-	my_array.push(email)
+    a = a + 1
+    if a < 10
+        email = "jean.dupont.0#{a}@email.fr"
+        puts email
+        my_array.push(email)
+    else
+        email = "jean.dupont.#{a}@email.fr"
+        puts email
+        my_array.push(email)
+    end    
 end
-puts my_array[0]
