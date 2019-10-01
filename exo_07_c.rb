0b78bfafd77c7758d4cd9f6d776689ea2a7d54cf
cmezouar <chloe-mez@hotmail.fr>
2019-10-01 15:32:49 +0200

    exo 7 b et c

---
+++ exo_07_c.rb
@@ -0,0 +1,10 @@
+user_name = gets.chomp
+puts user_name
+
+=begin 
+il n'y a pas de différence de résultat entre les trois programmes: ceux-ci demandent tous une entrée qui correspond 
+au prénom de l'utilisateur. Les programmes a et b comportent néanmoins plus d'indications pour l'utilisateur alors que le c n'indique rien 
+=end
+
+
+
