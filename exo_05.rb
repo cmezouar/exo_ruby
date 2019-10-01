9fc621832df4c9a21ec74fdee308ee4d30c7d7cb
assouev <bsngr.asv@gmail.com>
2019-10-01 14:44:43 +0200

    exo5

---
+++ exo_05.rb
@@ -0,0 +1,31 @@
+#affiche la chaine de caractères
+puts "On va compter le nombre d'heures de travail à THP"
+#affiche la chaine de caractères et effectue le calcul. 
+puts "Travail : #{10 * 5 * 11}"
+#idem
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+#affiche la chaine de caractères
+puts "Et en secondes ?"
+#calcule l'opération
+puts 10 * 5 * 11 * 60 * 60
+#affiche la chaine de caractères
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+#calcule 
+puts 3 + 2 < 5 - 7
+#chaine caractère + calcul
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+#idem
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+#chaine de caractère
+puts "Ok, c'est faux alors !"
+#chaine de caractères
+puts "C'est drôle ça, faisons-en plus :"
+#affiche la chaine de caractère et donne le resltat d'une inégalité (true/false) 
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+#pareil
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+#pareil
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+#pareil
+
+# "#{}" permet d'effectuer une operation à l'interieur d'une chaine de caractères. 
